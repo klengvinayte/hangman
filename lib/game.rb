be487e3bddd11455b702eb973652a85e5494520f
kristina_lengvinayte <klengvinayte@gmail.com>
2022-10-20 14:29:55 +0300

    Initial commit

---
+++ lib/game.rb
@@ -0,0 +1,70 @@
+class Game
+  attr_reader :letters
+
+  TOTAL_ERRORS_ALLOWD = 7
+
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  def errors_made
+    errors.length
+  end
+
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWD - errors_made
+  end
+
+  def letters_to_guess
+    result =
+      @letters.map do |letter|
+        if @user_guesses.include?(normalize_letter(letter))
+          letter
+        else
+          nil
+        end
+      end
+    result
+  end
+
+  def lost?
+    errors_allowed == 0
+  end
+
+  def over?
+    won? || lost?
+  end
+
+  def play!(letter)
+    normalized_letter = normalize_letter(letter)
+
+    if !over? && !@user_guesses.include?(normalized_letter)
+      @user_guesses << normalized_letter
+    end
+  end
+
+  def won?
+    (normalized_letters - @user_guesses).empty?
+  end
+
+  def word
+    @letters.join
+  end
+
+  private
+
+  def normalize_letter(letter)
+    return "Е" if letter == "Ё"
+    return "И" if letter == "Й"
+    letter
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+end
